a77e81271e6ebd9dd6c631cee5e096c0aabed542
Victor Hugo Fernandes Martins <victorhugofernandesmartins@gmail.com>
2026-04-02 16:06:36 -0300

    ✨ feat: tela inicial + JSON

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -5,8 +5,10 @@ import {
   ImageBackground,
   Dimensions,
   Image,
-  ScrollView
+  ScrollView,
+  
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
 import { useRef, useEffect, useState } from "react";
 
@@ -17,9 +19,12 @@ const width = Dimensions.get("window").width;
 export default function Home() {
   const carouselData = images.carrosel.midiasDestaque;
 
+// Referência para controlar o ScrollView (necessário para autoplay)
   const scrollRef = useRef(null);
+  
   const [index, setIndex] = useState(0);
 
+// Cria um intervalo que roda a cada 3 segundos
   useEffect(() => {
     const interval = setInterval(() => {
       const nextIndex = (index + 1) % carouselData.length;
@@ -29,39 +34,42 @@ export default function Home() {
         animated: true,
       });
 
-      setIndex(nextIndex);
+      setIndex(nextIndex);//atualiza estado
     }, 3000); // troca a cada 3 segundos
 
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); //limpa o intervalo
   }, [index]);
 
   return (
     <ImageBackground
       source={{
-        uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB4SeKEI96Inzn74JQzmNqXvxiObi7UDBYiQ&s",
+        uri: "https://estado.sc.gov.br/noticias/wp-content/uploads/sites/3/2022/05/ponte_hercilio_luz_completa_96_anos_com_programacao_especial_20220512_1673447418.jpg",
       }}
       style={styles.container}
       resizeMode="cover"
     >
-      <View style={styles.overlay}>
+       <SafeAreaView style={styles.overlay} edges={["top", "bottom"]}>
+
         
         <Text style={styles.description}>
           Explore as melhores atrações, descubra novos lugares e aproveite experiências incríveis com Floripass.
         </Text>
 
-        {/* 🎠 CARROSSEL COM AUTOPLAY */}
-        <View style={{ width: width * 0.9, height: 180 }}>
+        <View style={ styles.carouselWrapper }>
           <ScrollView
             ref={scrollRef}
             horizontal
             pagingEnabled
             showsHorizontalScrollIndicator={false}
+            scrollEnabled={true}
+            bounces={false}
+            overScrollMode="never"
           >
             {carouselData.map((item) => (
               <Image
                 key={item.id}
                 source={{ uri: item.url }}
-                style={styles.image}
+                style={styles.carouselImage}
               />
             ))}
           </ScrollView>
@@ -73,7 +81,7 @@ export default function Home() {
           </Link>
         </View>
 
-      </View>
+      </SafeAreaView>
     </ImageBackground>
   );
 }
@@ -81,37 +89,69 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height:"100%"
   },
 
-  overlay: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.5)",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
+overlay: {
+  flex: 1,
+  backgroundColor: "rgba(0,0,0,0.5)",
+  paddingHorizontal: 20,
+  justifyContent: "space-between", 
+  paddingBottom: 30,
+  overflow: "hidden"
+},
+
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 10,
   },
 
   description: {
     fontSize: 16,
-    color: "#fff",
+    color: "#ddd",
     textAlign: "center",
-    marginBottom: 30,
+    marginVertical: 10,
   },
 
+  carouselContainer: {
+    height: 200,
+    alignSelf: "center",
+  },
+
+  carouselWrapper: {
+  bottom: 120,
+  justifyContent: "center",
+},
+
+carouselImage: {
+  width: width * 0.9,     
+  height: 250,
+  borderRadius: 20,
+},
+
   image: {
-    width: width * 0.9,
-    height: 180,
-    borderRadius: 12,
-    marginRight: 10,
+    width: width * 1,
+    height: 200,
+    borderRadius: 15,
+    marginRight: 15,
   },
 
   navigationArea: {
-    marginTop: 20,
+    alignItems: "center",
   },
 
-  link: {
+  button: {
+    backgroundColor: "#6200ee",
     color: "#fff",
-    fontSize: 18,
-    textDecorationLine: "underline",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    fontSize: 16,
+    fontWeight: "bold",
+    overflow: "hidden",
   },
 });
