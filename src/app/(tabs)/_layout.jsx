5888b48b98e9907308e8d43a5525069898e759a1
Victor Hugo Fernandes Martins <victorhugofernandesmartins@gmail.com>
2026-04-03 17:33:10 -0300

    💄 feat: Estilização tab

--- src/app/(tabs)/_layout.jsx
+++ src/app/(tabs)/_layout.jsx
@@ -2,18 +2,46 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+
+        tabBarStyle: {
+          backgroundColor: "#020617",
+          borderTopWidth: 0, //tira divisão
+          height: 80,
+          paddingBottom: 12,
+          paddingTop: 10,
+        },
+
+        tabBarActiveTintColor: "#FFFFFF",
+        tabBarInactiveTintColor: "#64748B",
+
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: "700",
+          letterSpacing: 0.5,
+        },
+        tabBarItemStyle: {
+          borderRadius: 10,
+        },
+}}
+
       
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: "Home" }}
+        options={{
+          title: "Início",
+        }}
       />
 
       <Tabs.Screen
         name="ListaAtracoes"
-        options={{ title: "Atrações" }}
+        options={{
+          title: "Atrações",
+        }}
       />
-
     </Tabs>
   );
 }
